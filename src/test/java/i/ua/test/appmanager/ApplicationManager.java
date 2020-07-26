a22800e0a0665f3ed83bf881367df3648c785bd5
ragazzoua <ragazzo@ukr.net>
2020-07-26 13:18:04 +0300

    pull members up to the email helper

--- src/test/java/i/ua/test/appmanager/ApplicationManager.java
+++ src/test/java/i/ua/test/appmanager/ApplicationManager.java
@@ -4,13 +4,11 @@ import i.ua.test.model.Email;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager extends EmailHelper{
-    private WebDriver driver;
 
     public void init() {
         WebDriverManager.chromedriver().setup();
@@ -31,38 +29,6 @@ public class ApplicationManager extends EmailHelper{
         typeTo(email.getTo());
         typeSubject(email.getSubject());
         typeEmailText(email.getEmailText());
-    }
-
-    public void saveInDrafts() {
-        driver.findElement(By.xpath("//p[@class='send_container']//input[@name='save_in_drafts']")).click();
-    }
-
-    public void typeEmailText(String emailText) {
-        driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys(emailText);
-    }
-
-    public void typeSubject(String subject) {
-        driver.findElement(By.xpath("//input[@name='subject']")).sendKeys(subject);
-    }
-
-    public void typeTo(String to) {
-        driver.findElement(By.xpath("//textarea[@id='to']")).sendKeys(to);
-    }
-
-    public void clickCreateMessage() {
-        driver.findElement(By.xpath("//p[@class='make_message']//a")).click();
-    }
-
-    public void clickDraft() {
-        driver.findElement(By.xpath("/html/body/div[2]/div[6]/div[2]/div[1]/div[2]/div[3]/ul/li[3]/a")).click();
-    }
-
-    public void selectDraft(int index) {
-        driver.findElements(By.xpath("//input[@type='checkbox']")).get(index).click();
-    }
-
-    public void clickDeleteDraft() {
-        driver.findElement(By.xpath("//div[@id='fieldset1']//span[@class='button l_r del']")).click();
     }
 
     public void acceptAllert() {
--- src/test/java/i/ua/test/appmanager/EmailHelper.java
+++ src/test/java/i/ua/test/appmanager/EmailHelper.java
@@ -1,4 +1,40 @@
 package i.ua.test.appmanager;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class EmailHelper {
+    protected WebDriver driver;
+
+    public void saveInDrafts() {
+        driver.findElement(By.xpath("//p[@class='send_container']//input[@name='save_in_drafts']")).click();
+    }
+
+    public void typeEmailText(String emailText) {
+        driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys(emailText);
+    }
+
+    public void typeSubject(String subject) {
+        driver.findElement(By.xpath("//input[@name='subject']")).sendKeys(subject);
+    }
+
+    public void typeTo(String to) {
+        driver.findElement(By.xpath("//textarea[@id='to']")).sendKeys(to);
+    }
+
+    public void clickDraft() {
+        driver.findElement(By.xpath("/html/body/div[2]/div[6]/div[2]/div[1]/div[2]/div[3]/ul/li[3]/a")).click();
+    }
+
+    public void selectDraft(int index) {
+        driver.findElements(By.xpath("//input[@type='checkbox']")).get(index).click();
+    }
+
+    public void clickDeleteDraft() {
+        driver.findElement(By.xpath("//div[@id='fieldset1']//span[@class='button l_r del']")).click();
+    }
+
+    public void clickCreateMessage() {
+        driver.findElement(By.xpath("//p[@class='make_message']//a")).click();
+    }
 }
