cbc6aa15a4a6182f8bbefee881267fb40271df7f
ragazzoua <ragazzo@ukr.net>
2020-07-27 22:16:08 +0300

    Parametrized test

--- src/test/java/i/ua/test/tests/CreateDraftTest.java
+++ src/test/java/i/ua/test/tests/CreateDraftTest.java
@@ -1,15 +1,29 @@
 package i.ua.test.tests;
 
 import i.ua.test.model.Email;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class CreateDraftTest extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validEmails(){
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"test@ukr.net", "test@ukr.net", "test@ukr.net"});
+        list.add(new Object[]{"test2@ukr.net", "test2@ukr.net", "test2@ukr.net"});
+        list.add(new Object[]{"test3@ukr.net", "test3@ukr.net", "test3@ukr.net"});
+        return list.iterator();
+    }
 
-    @Test
-    public void userCanCreateDraft() {
+
+    @Test(dataProvider = "validEmails")
+    public void userCanCreateDraft(Email email) {
         app.getEmailHelper().clickCreateMessage();
-        app.getEmailHelper().createEmail(new Email("test@ukr.net", "test@ukr.net", "test@ukr.net"));
+        app.getEmailHelper().createEmail(new Email(email.getTo(), email.getSubject(), email.getEmailText()));
         app.getEmailHelper().clickSaveInDrafts();
     }
 
