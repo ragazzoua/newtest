660f41a80f24e8cf6e2567dbb333f1e8feab14cf
ragazzoua <ragazzo@ukr.net>
2020-07-27 21:13:09 +0300

    browser choose added

--- src/test/java/i/ua/test/appmanager/ApplicationManager.java
+++ src/test/java/i/ua/test/appmanager/ApplicationManager.java
@@ -3,6 +3,10 @@ package i.ua.test.appmanager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +16,23 @@ public class ApplicationManager {
     private Navigationhelper navigationhelper;
     private EmailHelper emailHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         emailHelper = new EmailHelper(driver);
--- src/test/java/i/ua/test/tests/TestBase.java
+++ src/test/java/i/ua/test/tests/TestBase.java
@@ -1,12 +1,13 @@
 package i.ua.test.tests;
 
 import i.ua.test.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeTest
     public void setupTest() {
